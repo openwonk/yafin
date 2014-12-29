546547972e33c8923ab6f5520537a62893358231
openwonk <bryan@neoty.pe>
2014-12-28 22:57:17 -0800

    yahoo finance

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	// RequestData("YHOO")
+	JSONize("YHOO")
+}
+
+func RequestData(symbol string) {
+	resp, err := http.Get("http://ichart.finance.yahoo.com/table.csv?s=" + symbol)
+	check(err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	check(err)
+
+	// ioutil.TempDir(dir, prefix)
+	check(ioutil.WriteFile("data."+strings.ToLower(symbol)+".csv", body, 0644))
+}
+
+func JSONize(symbol string) {
+	filepath := "data." + strings.ToLower(symbol) + ".csv"
+
+	data, err := ioutil.ReadFile(filepath)
+	check(err)
+
+	fmt.Println(string(data))
+
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
