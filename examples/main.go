b91e6e1e8dc50cd024779465e4b35b5b04585030
openwonk <bryan@neoty.pe>
2014-12-29 20:10:37 -0800

    portfolio testing

--- examples/main.go
+++ examples/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	yafin "github.com/openwonk/yafin.go"
+// yafin "github.com/openwonk/yafin.go"
+// "encoding/csv"
+// "encoding/json"
+// "fmt"
+// "os"
+// "strconv"
+// "strings"
 )
 
 func main() {
 	// yafin.RequestData("YHOO")
-	yafin.JSONize("YHOO")
+	// yafin.JSONize("YHOO")
+
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package yafin
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -21,30 +23,80 @@ func RequestData(symbol string) {
 	check(ioutil.WriteFile("data."+strings.ToLower(symbol)+".csv", body, 0644))
 }
 
-func JSONize(symbol string) {
-	// filepath := "data." + strings.ToLower(symbol) + ".csv"
-
-	// data, err := ioutil.ReadFile(filepath)
-	// check(err)
-
-	// fmt.Println(string(data))
-
-	// TODO: iterate through CSV and instert into JSON struct
-
+func JSONize(symbol string) Stock {
 	data, err := os.Open("data." + strings.ToLower(symbol) + ".csv")
 	check(err)
-
 	defer data.Close()
 
 	reader := csv.NewReader(data)
+	reader.FieldsPerRecord = -1 // see the Reader struct information below
 
 	raw, err := reader.ReadAll()
 	check(err)
 
+	var oneSession TradingSession
+	var multiSessions []TradingSession
+
+	// TODO: skip first row (headers)... maybe if key > 0?
 	for _, row := range raw {
-		fmt.Printf(">> %s", row[0])
+		// fmt.Printf("%s %s\n", row[0], row[1])
+		oneSession.Date = row[0]
+		oneSession.Open, _ = strconv.ParseFloat(row[1], 64)  // strconv.Atoi(row[1])
+		oneSession.High, _ = strconv.ParseFloat(row[2], 64)  // strconv.Atoi(row[2])
+		oneSession.Low, _ = strconv.ParseFloat(row[3], 64)   // strconv.Atoi(row[3])
+		oneSession.Close, _ = strconv.ParseFloat(row[4], 64) // strconv.Atoi(row[4])
+		oneSession.Volume, _ = strconv.Atoi(row[5])
+		oneSession.AdjClose, _ = strconv.ParseFloat(row[6], 64) // strconv.Atoi(row[6])
+		multiSessions = append(multiSessions, oneSession)
+
 	}
 
+	// StockHistory := map[string][]TradingSession{symbol: multiSessions}
+	// MarketHistory := map[string][]TradingSession{symbol: multiSessions}
+	singleStock := Stock{Name: strings.ToUpper(symbol), History: multiSessions}
+
+	jsonData, err := json.Marshal(singleStock)
+	check(err)
+
+	// fmt.Println(string(jsonData))
+
+	jsonFile, err := os.Create("data." + symbol + ".json")
+	check(err)
+
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+
+	return Stock
+
+}
+
+func GroupStocks(symbols []string) {
+	var m Market
+
+	for _, v := range symbols {
+		RequestData(v)
+		s := JSONize(v)
+		m = append(m, s)
+	}
+}
+
+type Market []Stock
+
+type Stock struct {
+	Name    string
+	History []TradingSession
+}
+
+type TradingSession struct {
+	Date     string
+	Open     float64
+	High     float64
+	Low      float64
+	Close    float64
+	Volume   int
+	AdjClose float64
 }
 
 func check(err error) {
